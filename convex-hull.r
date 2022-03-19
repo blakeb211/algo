5e67a175f03f824ed24f97b1a251ff83b339145a
blake <blake.elink@gmail.com>
2022-03-19 11:51:35 -0400

    commented and organized the R-version of convexhull

--- convex-hull.r
+++ convex-hull.r
@@ -1,67 +1,75 @@
 library(magrittr)
 library(ggplot2)
-# clear objects
-rm(list = ls())
-pts = list()
- length(pts) = 30 
+#######################################
+# Setup Environment
+#######################################
+rm(list = ls()) # clear objects
+NUMPTS = 30
+RNDLOW = 25
+RNDHIGH = 125
+#######################################
+# Functions 
+#######################################
 
+# Generate a random number
 rnd = function() {
-  # random number in specified range
-  sample(25:125, 1)
+  sample(RNDLOW:RNDHIGH, 1)
 }
 
+# Fill a list with random 2x1 matrices
 generate_rand_2vecs = function(pts) {
   for (i in 1:length(pts)) {
     pts[[i]] = c(rnd(), rnd()) %>% matrix
   }
   pts
 }
-pts = generate_rand_2vecs(pts)
-df = data.frame(data = pts)
-# remove any duplicate points, which will break the algo
-df = df[!duplicated(df),]
-df = df %>% t
-rownames(df) = 1:length(df[,1])
-colnames(df) = c("x", "y")
-rm(pts)
-print(df)
-df = data.frame(df)
 
-
+# Select a point from a Nx2 dataframe and return as 2-vector
 selectV2 = function(df, i) {
   df[i, ] %>% t %>% as.vector(.,mode="double")
 }
+
+# Calculate Euclidean norm of a vector
 norm_vec <- function(x) { sqrt(sum(x^2)) }
+
+# Convert degrees to radians
 deg2rad = function(x) {
   x * pi / 180.0 
 }
+
+# Convert radians to degrees
 rad2deg = function(x) {
   x * 180.0 / pi 
 }
+
+# Create a clockwise rotation matrix given an angle in degrees
 rotMat = function(ang) {
-  # create clockwise rotation matrix given an angle in degrees
-  # 1st convert degrees to radians
+  # First convert degrees to radians
   ang = deg2rad(ang)
-  # since the below is a counterclockwise rotation matrix,
-  # convert to angle from ccw to cw
+  # Since our formula yields a counter-clockwise rotation matrix,
+  # convert from counter-clockwise to clockwise
   ang = 2 * pi - ang
   res = matrix(c(cos(ang), -sin(ang), sin(ang),
                  cos(ang)), ncol = 2, byrow = TRUE)
   return(res)
 }
+
+# Sort a dataframe primarily by x and secondarily by y.
 sort.less.than = function(df) {
-  #return(df[order(df$x,df$y),])
   df = df[order(df$y,decreasing = TRUE),]
   df = df[order(df$x,decreasing = FALSE),]
   return(df)
-  #return(df[order(df$x,df$y),])
 }
+
+# Sort a dataframe primarily by x and secondarily by y.
 sort.greater.than = function(pts) {
   df = df[order(df$y, decreasing = FALSE),]
   df = df[order(df$x, decreasing = TRUE),]
   return(df)
   #return(df[order(df$x,df$y,decreasing = TRUE),])
 }
+
+# Calculate angle formed by 3 points
 angle.formed = function(a,b,c) {
   # angle in degrees formed by three 2-vectors
   stopifnot(length(a) == 2 && typeof(a) == "double")   
@@ -73,6 +81,14 @@ angle.formed = function(a,b,c) {
   angRad = acos(dp / norm_vec(ba) / norm_vec(bc))
   return(angRad %>% rad2deg)
 }
+
+# Determine if 3 points create a left turn by creating
+# a vector from the first two the second point, then rotating
+# it 90 degrees to the right to yield a fourth point.
+# Whether left hand turn is formed is determined by 
+# finding the angle between this new fourth point and the
+# last two input points. If the angle is > 90 degrees, it is a 
+# left hand turn.
 is.left.turn = function(a,b,c) {
   stopifnot(length(a) == 2 && typeof(a) == "double")   
   stopifnot(length(b) == 2 && typeof(b) == "double")   
@@ -84,10 +100,14 @@ is.left.turn = function(a,b,c) {
   if(is.nan(angformed)) return(TRUE)
   return(angformed > 90.0)
 }
+
+# Add a point from the random point dataframe to the solution
 add.pt = function(df, soln, currIndex) {
   rbind(soln, selectV2(df,currIndex)) 
 }
 
+# Find the upper or lower path of the convex hull using Andrew's
+# algorithm
 solve.convex.hull = function(upper=TRUE) {
   soln = matrix(ncol=2,nrow=0)
   # sort by decreasing to create the top of the hull
@@ -101,12 +121,14 @@ solve.convex.hull = function(upper=TRUE) {
   # keep track of the next point from the sorted set to add to
   # the solution.
   curridx = 3
-  
-  
-  # solve top of hull
-  # add a point
-  # check if left turn
-  # if left turn, remove 2nd to last point in soln
+ 
+  # Loop through the sorted points
+  # Add a point
+  # Check if left turn
+  # If left turn, remove 2nd to last point in solution
+  # Note: Sometimes we have to remove two points in a row, 
+  # so left hand turn checking and removal must be done
+  # in their own loop.
   while (curridx <= length(df[, 1])) {
     soln = add.pt(df, soln, curridx)
     # Remove 2nd to last point if a left hand turn is 
@@ -130,28 +152,30 @@ solve.convex.hull = function(upper=TRUE) {
   data.frame(soln)
 }
 
+#######################################
+# Main 
+#######################################
 
-
-test.convex.hull = function() {
-  soln = matrix(ncol=2,nrow=0)
-  a = c(1,2); b = c(2,3.5); c = c(1.5,4.5);
-  soln = rbind(soln,a)
-  soln = rbind(soln,b)
-  soln = rbind(soln,c)
-  angle.formed(soln[1,],soln[2,],soln[3,])
-  is.left.turn(soln[1,],soln[2,],soln[3,])
-  
-  soln
-  lenSoln = length(soln[,1])
-  data.frame(soln)
-}
-
-#test.convex.hull()
+# create a list of random points
+pts = list()
+length(pts) = NUMPTS 
+pts = generate_rand_2vecs(pts)
+# create a NUMPTSx2 dataframe
+df = data.frame(data = pts)
+df = df[!duplicated(df),] # remove duplicates
+df = df %>% t
+rownames(df) = 1:length(df[,1])
+colnames(df) = c("x", "y")
+rm(pts)
+print(df)
+df = data.frame(df)
+# solve convex hull
 soln.upper = solve.convex.hull(upper=TRUE)
 soln.lower = solve.convex.hull(upper=FALSE)
 
-cols = c("blue" = "blue")
-# plotting the result
+#######################################
+# Visualize result
+#######################################
 ggplot() +
   layer(
     data = df,
