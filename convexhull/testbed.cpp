fc486f4c2ad7798ac7fe25fd885d0fed3185b99b
blake <blake.elink@gmail.com>
2022-03-20 15:40:51 -0400

    setting up testbed

--- convexhull/testbed.cpp
+++ convexhull/testbed.cpp
@@ -5,21 +5,32 @@
 
 using std::vector, std::cout, std::stoi, std::endl;
 
-int test_rnd(int lower, int upper) { return 42; }
+bool test_rnd() {
+  int ans = rand_num(1, 5);
+  if (ans >= 1 && ans <= 5) return true;
+  return false;
+}
+
+/*************************************************
+ * To add a test,
+ *  1) add the number of the test to
+ *     the third argument of the getopt()
+ *     function call.
+ *  2) add a case statement to match the new
+ *     argument
+ ************************************************/
 
 int main(int argc, char *const *argv) {
-  char * optarg = nullptr;
   char c = ' ';
   while ((c = getopt(argc, argv, "12")) != -1) {
     switch (c) {
       case '1': {
-        cout << "arg 1 given" << endl;
-        cout << "optarg:" << optarg << endl;
+        cout << "running test_rnd()" << endl;
+        return test_rnd();
         break;
       }
       case '2': {
         cout << "arg 2 given" << endl;
-        cout << "optarg:" << optarg << endl;
         break;
       }
     };
