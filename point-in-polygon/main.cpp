fba908cf2f5f2e9f08920e163a67a9840eff0b1e
blake <blake.elink@gmail.com>
2022-03-27 11:43:27 -0400

    generating and drawing random polygons correctly

--- point-in-polygon/main.cpp
+++ point-in-polygon/main.cpp
@@ -16,29 +16,46 @@ using std::cout, std::endl, std::vector, std::polar, std::arg;
 constexpr size_t NUM_PT = 11;
 constexpr float SCALE = 2;
 constexpr int WINDIM = 800;
+constexpr int NUM_POLY = 10;
 vector<P> bullets;
 vector<Poly> polys;
 
 //////////////////////////////////////////////////////////////
-// Functions that are only in this file, not the pip library 
+// Functions that are only in this file, not the pip library
 //////////////////////////////////////////////////////////////
 int rand_num(const int lower, const int upper) {
-  return ( rand() % (upper - lower + 1)) + lower; 
+  return (rand() % (upper - lower + 1)) + lower;
 }
 
 P create_rand_pt(int first, int last) {
-  auto num1 = rand_num(first,last);
-  auto num2 = rand_num(first,last);
-  return P{ static_cast<float>(num1), static_cast<float>(num2) };
+  auto num1 = rand_num(first, last);
+  auto num2 = rand_num(first, last);
+  return P{static_cast<float>(num1), static_cast<float>(num2)};
 }
 
 auto generate_poly() -> Poly {
-
+  Poly tmp;
+  const int nvert = rand_num(3, 10);
+  const double rad = 15.0;  // set poly scale
+  const double d_ang = 2 * M_PI / nvert;
+  for (int i = 0; i < nvert; i++) {
+    P _p = polar(rad, d_ang * (i + 1));
+    tmp.push_back(_p);
+  }
+  assert(tmp.size() == nvert);
+  return tmp;
 }
 
-auto generate_bullet() -> P {
-
+// shift all points of polygon by a point
+auto shift_poly(Poly poly, const P shift) -> Poly {
+  const auto sz = poly.size();
+  for (int i = 0; i < sz; i++) {
+    poly[i] += shift;
+  }
+  return poly;
 }
+
+auto generate_bullet() -> P { return P{}; }
 
 //////////////////////////////////////////////////////////////
 // main function
@@ -47,6 +64,17 @@ int main(int argc, char* argv[]) {
   // random points
   srand(time(NULL));
 
+  // generate polygons
+  polys.resize(NUM_POLY);
+
+  for (auto& p : polys) {
+    p = generate_poly();  // gen poly centered on 0,0
+    const C xshift = rand_num(WINDIM / SCALE * 0.10, WINDIM / SCALE * 0.90);
+    const C yshift = rand_num(WINDIM / SCALE * 0.10, WINDIM / SCALE * 0.75);
+    p = shift_poly(p, P{xshift, yshift});
+  }
+
+  SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "2");
   // create window and draw solution
   if (SDL_Init(SDL_INIT_VIDEO) == 0) {
     SDL_Window* window = NULL;
@@ -56,6 +84,7 @@ int main(int argc, char* argv[]) {
         0) {
       SDL_bool done = SDL_FALSE;
 
+      SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "2");
       SDL_RenderSetScale(renderer, SCALE, SCALE);
 
       while (!done) {
@@ -63,22 +92,28 @@ int main(int argc, char* argv[]) {
 
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
         SDL_RenderClear(renderer);
-        
-        // Draw polygons
-        //    SDL_SetRenderDrawColor(renderer, 100, 5, 255, SDL_ALPHA_OPAQUE);
-        //    auto num_lines = soln1.size() - 1;
-        //    // a---b---c----d
-        //    for (int i = 0; i < num_lines; i++) {
-        //      SDL_RenderDrawLine(renderer, soln1[i].x,
-        //      WINDIM/SCALE-soln1[i].y, \
-    //          soln1[i+1].x, WINDIM/SCALE-soln1[i+1].y);
-        //    }
 
+        // Draw polygons
+        // number of lines drawn equals number of vertices
+        SDL_SetRenderDrawColor(renderer, 10, 250, 10, SDL_ALPHA_OPAQUE);
+        for (const auto& p : polys) {
+          const auto sz = p.size();
+          // a---b---c
+          for (int i = 0; i < sz - 1; i++) {
+            SDL_RenderDrawLine(
+                renderer, round(p[i].X), round(WINDIM / SCALE - p[i].Y),
+                round(p[i + 1].X), round(WINDIM / SCALE - p[i + 1].Y));
+          }
+          // c----a
+          SDL_RenderDrawLine(renderer, round(p[sz - 1].X),
+                             round(WINDIM / SCALE - p[sz - 1].Y), round(p[0].X),
+                             round(WINDIM / SCALE - p[0].Y));
+        }
         // draw the bullets so they are on top
         //     SDL_SetRenderDrawColor(renderer, 255, 255, 255,
         //     SDL_ALPHA_OPAQUE); for (int i = 0; i < NUM_PT; i++)
-        //       SDL_RenderDrawPoint(renderer, pts[i].x, WINDIM/SCALE-pts[i].y);
-
+        //       SDL_RenderDrawPoint(renderer, pts[i].x,
+        //       WINDIM/SCALE-pts[i].y);
         SDL_RenderPresent(renderer);
 
         while (SDL_PollEvent(&event)) {
