ae43b2707af92eb2f419decb0312fda6d2f70149
blake <blake.elink@gmail.com>
2022-03-27 21:24:42 -0400

    updated template

--- templates/main-sdl.cpp
+++ templates/main-sdl.cpp
@@ -1,42 +1,97 @@
 #include "SDL.h"
 
+using std::cout, std::endl, std::vector, std::polar, std::arg;
 
-int main(int argc, char* argv[])
-{
-    if (SDL_Init(SDL_INIT_VIDEO) == 0) {
-        SDL_Window* window = NULL;
-        SDL_Renderer* renderer = NULL;
+//////////////////////////////////////////////////////////////
+// globals
+//////////////////////////////////////////////////////////////
+constexpr float SCALE = 4;
+constexpr int WINDIM = 800;
 
-        if (SDL_CreateWindowAndRenderer(640, 480, 0, &window, &renderer) == 0) {
-            SDL_bool done = SDL_FALSE;
+//////////////////////////////////////////////////////////////
+// Functions that are only in this file
+//////////////////////////////////////////////////////////////
+int rand_num(const int lower, const int upper) {
+  return (rand() % (upper - lower + 1)) + lower;
+}
 
-            while (!done) {
-                SDL_Event event;
+P create_rand_pt(int first, int last) {
+  auto num1 = rand_num(first, last);
+  auto num2 = rand_num(first, last);
+  return P{static_cast<float>(num1), static_cast<float>(num2)};
+}
 
-                SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-                SDL_RenderClear(renderer);
+//////////////////////////////////////////////////////////////
+// main function
+//////////////////////////////////////////////////////////////
+auto main(int argc, char* argv[]) -> int {
+  ////////////////////////////////////////////////////////////
+  // init
+  // /////////////////////////////////////////////////////////
+  srand(time(NULL));
 
-                SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
-                SDL_RenderDrawLine(renderer, 320, 200, 300, 240);
-                SDL_RenderDrawLine(renderer, 300, 240, 340, 240);
-                SDL_RenderDrawLine(renderer, 340, 240, 320, 200);
-                SDL_RenderPresent(renderer);
+  // create window and draw solution
+  if (SDL_Init(SDL_INIT_VIDEO) == 0) {
+    SDL_Window* window = NULL;
+    SDL_Renderer* renderer = NULL;
 
-                while (SDL_PollEvent(&event)) {
-                    if (event.type == SDL_QUIT) {
-                        done = SDL_TRUE;
-                    }
-                }
-            }
+    if (SDL_CreateWindowAndRenderer(WINDIM, WINDIM, 0, &window, &renderer) ==
+        0) {
+      SDL_bool done = SDL_FALSE;
+
+      SDL_RenderSetScale(renderer, SCALE, SCALE);
+
+      // main loop
+      while (!done) {
+        SDL_Event event;
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+        SDL_RenderClear(renderer);
+
+        // Draw polygons
+        // number of lines drawn equals number of vertices
+        SDL_SetRenderDrawColor(renderer, 10, 250, 10, SDL_ALPHA_OPAQUE);
+        for (size_t poly_idx = 0; poly_idx < NUM_POLY; poly_idx++) {
+          if (!poly_draw_flag[poly_idx]) continue;
+          const auto& p = polys[poly_idx];
+          const auto sz = p.size();
+          // draw sz-1 lines a---b---c
+          for (size_t i = 0; i < sz - 1; i++) {
+            SDL_RenderDrawLine(
+                renderer, round(p[i].X), round(WINDIM / SCALE) - round(p[i].Y),
+                round(p[i + 1].X), round(WINDIM / SCALE) - round(p[i + 1].Y));
+          }
+          // draw last line c----a
+          SDL_RenderDrawLine(renderer, round(p[sz - 1].X),
+                             round(WINDIM / SCALE) - round(p[sz - 1].Y),
+                             round(p[0].X),
+                             round(WINDIM / SCALE) - round(p[0].Y));
+        }
+        for (size_t bi = 0; bi < NUM_BOMB; bi++) {
+          SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+          SDL_RenderDrawPoint(renderer, round(bombs[bi].X),
+                              round(WINDIM / SCALE) - round(bombs[bi].Y + 2));
         }
 
-        if (renderer) {
-            SDL_DestroyRenderer(renderer);
+        SDL_RenderPresent(renderer);
+
+        while (SDL_PollEvent(&event)) {
+          if (event.type == SDL_QUIT) {
+            done = SDL_TRUE;
+          }
         }
-        if (window) {
-            SDL_DestroyWindow(window);
-        }
+        SDL_Delay(16);
+        if (0) break;
+      }  // end of main loop
     }
-    SDL_Quit();
-    return 0;
+
+    if (renderer) {
+      SDL_DestroyRenderer(renderer);
+    }
+    if (window) {
+      SDL_DestroyWindow(window);
+    }
+  }
+  SDL_Quit();
+  return 0;
 }
